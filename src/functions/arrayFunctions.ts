0387749c97ea649310c49b2e3c3e9f599b5c8504
Ishan Singh <nameste380@gmail.com>
2024-03-17 11:12:29 +0530

    addingFourFunctions

--- src/functions/arrayFunctions.ts
+++ src/functions/arrayFunctions.ts
@@ -13,17 +13,41 @@ module.exports = {
     //* Find the Max element value in array
     return Math.max(...array);
   },
-
+  //* Check is Array is Empty
   isEmpty: (array: number[]): boolean => {
     return array.length === 0;
   },
-
+  //* Remove Duplicates
   removeDuplicates: <T>(array: T[]): T[] => {
     return Array.from(new Set(array));
   },
-
+  //* Return array of difference
   arrayDifference: <T>(array1: T[], array2: T[]): T[] => {
     const set2 = new Set(array2);
     return array1.filter((item) => !set2.has(item));
+  },
+  //* Shuffle Array
+  shuffleArray: <T>(array: T[]): T[] => {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    return shuffledArray;
+  },
+  //* Find Min Element
+  minElement: (array: number[]): number => {
+    return Math.min(...array);
+  },
+  //* Find Max Element
+  maxElement: (array: number[]): number => {
+    return Math.max(...array);
+  },
+  //* Check is element preset in array
+  containsElement: <T>(array: T[], element: T): boolean => {
+    return array.includes(element);
   },
 };
